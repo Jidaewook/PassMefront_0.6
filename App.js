0ad350123b72ab95d1477075dfa9032ca98e4098
Jidaewook <psatdoctor@naver.com>
2020-05-01 10:53:37 +0900

    Basic Config<cacheFonts>
    여기까지 expo start 확인 유지

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import {AppLoading} from 'expo';
-import { Text, Image } from 'react-native';
+import * as Font from "expo-font";
+import { Text, Image, View } from 'react-native';
 import {Asset} from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
 
 //무조건 초기 설정에서 해줘야 하는 사항
 const cacheImage = images => 
@@ -13,6 +15,8 @@ const cacheImage = images =>
     }
   });
 
+const cacheFonts = fonts => 
+  fonts.map(font => [Font.loadAsync(font), Font.loadAsync(font)]);
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -21,11 +25,14 @@ export default function App() {
       "https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       require("./assets/splash.png")
     ]);
-      console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
   return isReady ? (
-    <Text>Ready!</Text>
+    <View>
+      <Text>Ready!</Text>
+    </View>
   ) : (
     <AppLoading
       startAsync={loadAsset}
