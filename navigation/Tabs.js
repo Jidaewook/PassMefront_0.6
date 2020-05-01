6985d6662d99db3988eaa8333034cd5c8cebcfc8
Jidaewook <psatdoctor@naver.com>
2020-05-01 11:52:34 +0900

    Tabs Config<TabBarIcon>

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Ionicons} from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import Post from "../screens/Post";
 import Search from "../screens/Search";
 import Activity from "../screens/Activity";
 import Mypage from "../screens/Mypage";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -13,14 +15,48 @@ const getHeaderName = route =>
     route?.state?.routeNames[route.state.index] || "Home";
 
 export default ({navigation, route}) => {
+
     useLayoutEffect(() => {
         const name = getHeaderName(route);
         navigation.setOptions({
             title: name
         });
     }, [route])
+
     return(
-        <Tabs.Navigator>
+        
+        <Tabs.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused}) => {
+                    let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+                    if(route.name === "Home"){
+                        iconName += "home"
+                    } else if (route.name === "Post") {
+                        iconName += "add"
+                    } else if (route.name === "Search") {
+                        iconName += "search"
+                    } else if (route.name === "Activity") {
+                        iconName += "barcode"
+                    } else if (route.name === "Mypage") {
+                        iconName += "person"
+                    }
+                    return (
+                        <Ionicons 
+                            name={iconName}
+                            color={focused ? "white" : "grey"}
+                            size={26}
+                        />
+                    );
+                }
+            })}  
+            tabBarOptions={{
+                showLabel: true,
+                style: {
+                    backgroundColor: "black",
+                    borderTopColor: "black"
+                }
+            }}
+        >
             <Tabs.Screen name="Home" component={Home}/>
             <Tabs.Screen name="Post" component={Post}/>
             <Tabs.Screen name="Search" component={Search}/>
