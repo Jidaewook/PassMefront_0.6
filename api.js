b0e6a7dd5eef88649790b3e1f9083d4bd2d931d2
Jidaewook <psatdoctor@naver.com>
2020-05-01 13:00:38 +0900

    Api Config<lectureApi called in Home>

---
+++ api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const makeRequest = (path) => 
+    axios.get(`http://localhost:7000${path}`);
+
+const getAnything = async (path, params = {}) => {
+    try{
+        const {
+            data: { results },
+            data
+        } = await makeRequest(path, params);
+        return [results || data, null];
+    } catch (e) {
+        console.log(e);
+        return [null, e];
+    }
+};
+
+export const lectureApi = {
+    ncs: () => getAnything("/lecture/ncs"),
+    psat: () => getAnything("/lecture/psat"),
+    ncsDetail: id => getAnything(`/lecture/ncs/${id}`),
+    psatDetail: id => getAnything(`/lecture/psat/${id}`)
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
+import {lectureApi} from '../api';
 
-export default ({navigation}) => (
-    <View style={{flex: 1, backgroundColor: "black"}}>
-        <Text>Home</Text>
-        <Button 
-            onPress={() => navigation.navigate("Detail")}
-            title="Go to Detail"
-        />
-    </View>
+export default () => {
+    const [lecures, setLectures] = useState({
+        ncs: [],
+        psat: [],
+        ncsError: null,
+        psatError: null
 
-)
+    });
+
+    const getData = async () => {
+        const [ncs, ncsError] = await lectureApi.ncs();
+        const [psat, psatError] = await lectureApi.psat();
+
+        setLectures({
+            ncs,
+            ncsError,
+            psat,
+            psatError
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <View style={{flex: 1, backgroundColor: "black"}}>
+            <Text style={{color: "white"}}>
+                ncsCount is : {lecures.ncs?.length}
+            </Text>
+            <Text style={{color: "white"}}>
+                psatCount is : {lecures.psat?.length}
+            </Text>
+        </View>
+    )
+};
+
+
+
