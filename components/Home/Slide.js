055abae797b9c425b4c590d2fa673049f2d58633
Jidaewook <psatdoctor@naver.com>
2020-05-05 11:35:37 +0900

    Home Slide Config<Slide Components Config#2>

--- components/Home/Slide.js
+++ components/Home/Slide.js
@@ -1,24 +1,49 @@
 import React from 'react'
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
-import {Dimensions, Image} from "react-native";
 import {apiImage} from "../../api";
 
-const {width: WIDTH, height: HEIGHT} = Dimensions.get("screen");
 
 const Container = styled.View`
-    width: ${WIDTH}px;
-    height: ${HEIGHT / 4}px;
+    width: 100%;
+    height: 100%;
 `;
 
 const BG = styled.Image`
     width: 100%;
     height: 100%;
+    opacity: 0.6;
+    position: absolute;
+`;
+
+const Content = styled.View`
+    flex-direction: row;
+`;
+
+const Data = styled.View`
+    width: 50%;
+`;
+
+const Title = styled.Text`
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+`;
+
+const Desc = styled.Text`
+    color: white;
+    opacity: 0.7;
 `;
 
 const Slide = ({id, title, desc, url, thumbnail, uploadDate}) => (
     <Container>
-        <BG source={{ uri: apiImage(thumbnail) }} />
+        <BG source={{ url: apiImage(thumbnail) }} />
+        <Content>
+            <Data>
+                <Title>{title}</Title>
+                <Desc>{desc}</Desc>
+            </Data>
+        </Content>
     </Container>
 );
 
--- screens/Home/HomePresenter.js
+++ screens/Home/HomePresenter.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, View, Dimensions } from "react-native";
 import Slide from '../../components/Home/Slide';
+
+const {width: WIDTH, height: HEIGHT} = Dimensions.get("screen");
 
 const Container = styled.View`
     flex:1;
@@ -10,6 +12,10 @@ const Container = styled.View`
     justify-content: center;
 `;
 
+const SliderContainer = styled.View`
+    width: ${WIDTH}px;
+    height: ${HEIGHT / 4}px;
+`;
 
 
 export default ({loading, psat}) => (
@@ -17,7 +23,7 @@ export default ({loading, psat}) => (
         {loading ? (
             <ActivityIndicator color="white" size="large" />
         ): (
-            <>
+            <SliderContainer>
                 <Swiper controlsEnabled={false} loop timeout={3}>
                     {psat.map(item => (
                         <Slide
@@ -30,7 +36,7 @@ export default ({loading, psat}) => (
                         />
                     ))}
                 </Swiper>
-            </>      
+            </SliderContainer>      
         )}
 
     </Container>
